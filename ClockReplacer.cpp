bfcb53c8ff89ab5ef3e42116f32f8cfa2249c2ab
tuzi06 <damienwang21@gmail.com>
2021-06-08 00:32:53 -0700

    rewrite clock replace by add oldest queue

--- ClockReplacer.cpp
+++ ClockReplacer.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <queue>
 using namespace std;
 
 class current {
 private:
-    bool replace;
+    bool candidate;
     int pinCount;
     
 public:
     current(){
-        replace = false;
+        candidate = false;
         pinCount=0; //unpin
     }
-    void replacement(){
-        replace = true;
+    void replace(){
+        candidate = true;
     }
     void pin(){
         pinCount++;
@@ -27,34 +28,54 @@ public:
     int getPinCount(){
         return pinCount;
     }
+
+    bool getCandidate(){
+        return candidate;
+    }
     
 };
+
+queue<int> removePinFromQueue(queue<int> q, int pin){
+    int n=q.size();
+    for(int i=0; i<n;i++){
+        int intval =q.front();
+        q.pop();
+        if(intval != pin)
+            q.push(intval);
+    }
+    return q;
+}
 
 int main(){
     ifstream fin("input.txt");
     ofstream fout("output.txt");
 
-    int count,j=0;
-    int testcount =0;
+    int count;
+    queue<int> oldest;
+
     fin>>count;
-    current frames [count];
+    current buffer[count];
     while (fin.eof() == false){
-        testcount++;
         string currentStep;
         fin>>currentStep;
-        if(currentStep[0] == 'U')
-            frames[currentStep[1]].unpin();
-
-        else if(currentStep[0] == 'P')
-            frames[currentStep[1]-49].pin();
+        if(currentStep[0] == 'U'){
+            buffer[currentStep[1]-49].unpin();
+            if( buffer[currentStep[1]-49].getPinCount() == 0)
+                oldest.push(currentStep[1]-48);
+        }
+        else if(currentStep[0] == 'P'){
+            buffer[currentStep[1]-49].pin();
+            oldest= removePinFromQueue(oldest,currentStep[1]-48);
+        }
 
         else if(currentStep[0] == 'R'){
-            while(frames[j].getPinCount()!=0)
-                j++;
-            int i=j+1;
-            j++;
-            fout<<i<<"\t";
+            buffer[currentStep[1]-49].replace();
+            int refer = oldest.front();
+            fout<<refer<<"\t";
+            oldest.pop();
+            oldest.push(refer);
         }
     }
+    
     return 0;
 }
