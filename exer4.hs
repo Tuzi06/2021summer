f7e2dc6eff4df69a10656ae0895a69ebd9b0778d
tuzi06 <damienwang21@gmail.com>
2021-06-09 14:49:57 -0700

    finish fuck

--- exer4.hs
+++ exer4.hs
@@ -1,7 +1,26 @@
-pascal :: Int ->[Int]
+import Data.Maybe
+pascal :: Int->[Int]
 pascal 0 = [1]
 pascal n = [1] ++ map add (zip prev (tail prev)) ++[1]
     where   prev = pascal (n-1)
             add :: (Int, Int)-> Int
             add (x,y) = x+y
+
 --pointfree addition
+addPair :: (Integer ,Integer )-> Integer 
+addPair = uncurry (+)
+
+--pointfree filtering
+withoutZeros :: (Eq a,Num a)=> [a]->[a]
+withoutZeros = filter ( /=0)
+
+--search? maybe?
+findElt :: (Eq t, Num a) => t -> [t] -> Maybe a
+findElt x [] = Nothing
+findElt x (y:ys)=  listToMaybe (search 0  x (y:ys))
+                        where 
+                            search :: (Eq t, Num a) => a -> t-> [t] ->[a] 
+                            search index x [] = []
+                            search index x (y:ys)
+                                |   x==y          = [index]
+                                |   otherwise     = search (index+1) x ys
--- test.hs
+++ test.hs
@@ -1,8 +1,11 @@
+import Data.Maybe 
+half :: Fractional a => a -> a
 half a=a/2
+isBig :: (Ord a, Num a) => a -> Bool
 isBig a = a > 100
 listify x y = [x, y]
 myand False _=False
-myand True  a=a 
+myand True  a=a
 
 
 mysum [] =0
@@ -11,3 +14,13 @@ mysum (x:xs) = x+ mysum xs
 check (a:b:c:xs) --string
     |   a:b:c:[] == "520"   =True
     |   otherwise           =False
+
+
+
+
+search index x [] = []
+search index x (y:ys)
+    |   x==y            = [index]
+    |   otherwise       = search (index+1) x ys
+
+isNull (y:ys) = null [y]
