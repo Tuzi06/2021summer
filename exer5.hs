e6203bae1589840a1e1374cecaf071d290bb664f
tuzi06 <damienwang21@gmail.com>
2021-06-14 00:33:59 -0700

    just I/O left

--- exer5.hs
+++ exer5.hs
@@ -1,5 +1,5 @@
+import Data.Ratio
 --Buit-in funciton
-
 myIterate :: (a -> a) -> a -> [a]
 myIterate f n = n: myIterate f (f n)
 
@@ -8,3 +8,26 @@ mySplitAt 0 (y:ys) = ([],(y:ys))
 mySplitAt x (y:ys) = let (a,b) = mySplitAt (x-1) ys
                     in ([y] ++ a, b)
 
+--Rational Numbers
+rationalSum :: Int -> [Ratio Int]
+rationalSum n = getRatio 1 (n-1)
+                where
+                    getRatio :: Int -> Int -> [Ratio Int] 
+                    getRatio x 1 = [x % 1] 
+                    getRatio x y = [x % y] ++ getRatio (x+1) (y-1)
+
+--Lowest Terms Only
+rationalSumLowest :: Int -> [Ratio Int]
+rationalSumLowest n = getRatio 1 (n-1)
+                where
+                    getRatio :: Int -> Int -> [Ratio Int] 
+                    getRatio x 1 = [x % 1] 
+                    getRatio x y 
+                        |   gcd x y == 1    =[x % y] ++ getRatio (x+1) (y-1)
+                        |   otherwise       = getRatio (x+1) (y-1)
+
+-- All Rational Numbers
+rationals :: [Ratio Int]
+rationals = rationals' 2
+            where   rationals' :: Int -> [Ratio Int] 
+                    rationals' n = rationalSumLowest n ++ rationals' (n+1)
