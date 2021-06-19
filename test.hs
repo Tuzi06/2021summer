810ed832b3f2eb9d6150f73e4960019f27a63f90
tuzi06 <damienwang21@gmail.com>
2021-06-19 02:16:04 -0700

    still cannot figure out findpassowrd

--- test.hs
+++ test.hs
@@ -1,26 +1,38 @@
-import Data.Maybe 
-half :: Fractional a => a -> a
-half a=a/2
-isBig :: (Ord a, Num a) => a -> Bool
-isBig a = a > 100
-listify x y = [x, y]
-myand False _=False
-myand True  a=a
+
+import RainbowAssign
+import qualified Data.Map as Map
+filename :: FilePath
+pwLength = 8            -- length of each password
+nLetters = 5            -- number of letters to use in passwords: 5 -> a-e
+width = 40              -- length of each chain in the table
+height = 1000           -- number of "rows" in the table
+filename = "table.txt"  -- filename to store the table
+
+--hashing and reducing
+modular :: Int -> Int
+modular hash =hash `mod` nLetters
+
+divide:: Int->Int
+divide hash  = hash`div` nLetters
+
+getList:: Int -> Int ->[Int]->[Int]
+getList hash time []= getList (divide hash) (time-1) [modular hash]
+getList hash time (y:ys)
+    |   time >= 0     = getList(divide hash) (time -1) [modular hash]++(y:ys)
+    |   otherwise     = []
+
+pwReduce :: Hash  -> Passwd 
+pwReduce hash = map toLetter (getList (fromEnum hash) pwLength [])
+
+convert :: Int -> Passwd->Hash
+convert 0 password = pwHash password
+convert x password = pwHash(pwReduce(convert (x-1) password))
 
 
-mysum [] =0
-mysum (x:xs) = x+ mysum xs
+makeList ::Int ->[Passwd]->[(Hash,Passwd)]
+makeList x [] = []
+makeList x (y:ys) = [((convert x y),y)] ++ (makeList x ys)
 
-check (a:b:c:xs) --string
-    |   a:b:c:[] == "520"   =True
-    |   otherwise           =False
-
-
-
-
-search index x [] = []
-search index x (y:ys)
-    |   x==y            = [index]
-    |   otherwise       = search (index+1) x ys
-
-isNull (y:ys) = null [y]
+rainbowTable:: Int -> [Passwd] -> Map.Map Hash Passwd
+rainbowTable x [] = Map.fromList ([])
+rainbowTable x (y:ys)= Map.fromList (makeList x (y:ys))
