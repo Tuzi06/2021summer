4f16ba3fe064b241d344cf1ee430120334441e3b
tuzi06 <damienwang21@gmail.com>
2021-07-15 23:51:22 -0700

    finish find elt

--- exer/exer8/src/find.rs
+++ exer/exer8/src/find.rs
@@ -1,4 +1,13 @@
 pub fn find_elt<T: Eq>(values: &Vec<T>, elt: T) -> Option<usize> {
     // TODO
-    
+    let mut i =0;
+    for value in values{
+        if *value == elt{
+            return Some(i);
+        }
+        else {
+            i=i+1;
+        }
+    }
+    return None;
 }
