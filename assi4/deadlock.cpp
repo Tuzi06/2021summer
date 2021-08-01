316c1dee4faee1c43716c103e7ead2b248001d9b
tuzi06 <damienwang21@gmail.com>
2021-08-01 07:45:44 -0700

    dd

---
+++ assi4/deadlock.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+
+int main(){
+    ifstream fin ("input.txt");
+    cout<<"good"<<endl;
+    // while (fin.eof() == false){
+    //     string s;
+    //     fin >>s;
+    //     cout<< s<<endl;
+    // }
+
+    return 0;
+}
